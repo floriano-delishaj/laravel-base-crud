efa5411185bbc0d683547f762b6312059c5aafcb
floriano-delishaj <delishaj.floriano@live.it>
2022-03-09 16:01:23 +0100

    bonus done with bootstrap model

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -44,55 +44,61 @@
             </div>
 
             <div class="d-flex">
-                <a class="btn btn-primary px-4 me-3" href="{{route('comics.edit', $comic['id'])}}">Edit</a>
+                <a class="btn btn-primary px-4 me-3" href="{{route('comics.edit', $comic->id)}}">Edit</a>
 
-                <form  action="{{route('comics.destroy', $comic->id)}}" method="post" id="form_delete">
+                <!-- Button trigger modal -->
+                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#reg-modal">
+                    Delete
+                </button>
+               <form  action="{{route('comics.destroy', $comic->id)}}" method="post" id="form_delete">
                     @csrf
                     @method('delete')
-                    <button class="btn btn-danger px-4" type="submit">Delete</button>
-                </form>
-
-                <script>
-                    const form = document.getElementById('form_delete');
-                    form.addEventListener('submit', function (e) {
-                        e.preventDefault();
-
-                        const choise = confirm('Sei sicuro di voler cancellare l\'elemento? #{{$comic->id}}');
-
-                        if(choise) {
-                            form.submit()
-                        }
-                    })
-                </script>
+                        <!-- Modal -->
+                        <div class="modal fade" id="reg-modal" tabindex="-1" aria-labelledby="modal-title" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="modal-title">Delete comic #{{$comic->id}}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Are you sure you want to delete comic  <b>#{{$comic->id}}</b> ? You will not be able to go back
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+               </form>
             </div>
 
         </div>
         <hr />
         <div class="container d-flex justify-content-center">
-                <div class="col-12 col-lg-6">
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th scope="col"><h3 class="fw-bold">Specs</h3></th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row" class="w-25">Series:</th>
-                            <td><span class="text-uppercase">{{$comic['series']}}</span></td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="w-25">U.S. Price:</th>
-                            <td>{{$comic['price']}}</td>
-                        </tr>
-                        <tr>
-                            <th scope="row" class="w-25">On Sale Date:</th>
-                            <td>{{$comic['sale_date']}}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col"><h3 class="fw-bold">Specs</h3></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row" class="w-25">Series:</th>
+                    <td><span class="text-uppercase">{{$comic['series']}}</span></td>
+                </tr>
+                <tr>
+                    <th scope="row" class="w-25">U.S. Price:</th>
+                    <td>{{$comic['price']}}</td>
+                </tr>
+                <tr>
+                    <th scope="row" class="w-25">On Sale Date:</th>
+                    <td>{{$comic['sale_date']}}</td>
+                </tr>
+                </tbody>
+            </table>
+
         </div>
     </section>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="{{ asset('js/app.js') }}"></script>
     <title>@yield('pageTitle') | DC</title>
