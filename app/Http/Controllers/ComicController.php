498b27e40de6ed9dfa2a4e56aa32b63ed8c94108
floriano-delishaj <delishaj.floriano@live.it>
2022-03-09 16:43:06 +0100

    fix

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -46,6 +46,7 @@ class ComicController extends Controller
     public function store(Request $request)
     {
         $comic = $request->all();
+
         $newComic = Comic::create($comic);
 
 /*        $newComic = new Comic();
@@ -68,40 +69,19 @@ class ComicController extends Controller
         return view('comics.show',compact('comic'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Comic  $comic
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Comic $comic)
     {
         return view('comics.edit', compact('comic'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comic  $comic
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Comic $comic)
     {
-        $data = $request->all();
-
         //esegue sia il fill() che il save()
-        $comic->update($data);
+        $comic->update($request->all());
 
         return redirect()->route('comics.show', compact('comic'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Comic  $comic
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Comic $comic)
     {
         $comic->delete();
